a74d15718e096d1a5765eb8be54b45cf7d473e37
Futral <FUTRALMC@ethyl.net>
2022-03-07 19:44:16 -0500

    -m

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,10 @@
 function Flights() {
 
+    function calculateNumberOfFlights(Passengers, Capacity) {
+        let flights = (Passengers / Capacity);
+        return flights;
+    }
+
+    console.log(calculateNumberOfFlights(1000, 100));
 }
 
